0365fc12be8c832640205ab1bfb9ec726e26de9d
Vivian Phung <dev+github@vivianphung.com>
2023-08-24 20:43:03 -0400

    init

--- README.md
+++ README.md
@@ -13,9 +13,10 @@ npm login
 ## [Init](https://docs.npmjs.com/creating-and-publishing-scoped-public-packages)
 
 scoped to org or user:
+
 ```sh
 npm init --scope=@<my-org or my-username>
-``` 
+```
 
 ## Update version
 
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,12 @@
-export declare function exLibFunction(name: string): string;
+import { AuthMethod } from "@lit-protocol/types";
+declare class SnowballAuth {
+    private litAuthClient;
+    private webAuthnProvider;
+    constructor(apiKey: string);
+    registerPasskey(username: string): Promise<{
+        pkpEthAddress: string;
+        pkpPublicKey: string;
+    }>;
+    authenticatePasskey(): Promise<AuthMethod>;
+}
+export default SnowballAuth;
